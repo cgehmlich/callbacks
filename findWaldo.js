f2f7b10304ee447f9e0cc230f001a3ee1aa920e2
cgehmlich <cgehmlich@gmail.com>
2018-10-25 18:52:50 +0000

    changed to forEach

--- findWaldo.js
+++ findWaldo.js
@@ -1,16 +1,10 @@
-// The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      cb = i;
-      found(cb);   // execute callback
-    }
+var arr = ["Alice", "Bob", "Waldo", "Winston"]
+arr.forEach(function(name){
+  if (name === "Waldo") {
+    cb = arr.indexOf(name);
+    actionWhenFound(cb); // execute callback
   }
-}
-
+});
 function actionWhenFound() {
-
   console.log("Found him at index " + cb);
 }
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
